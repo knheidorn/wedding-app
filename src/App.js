c59552d1232eeb5475f758f80cc496490615001a
Kimberly Heidorn <heidorkn@gmail.com>
2019-10-11 11:56:30 -0700

    Building out landing page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import './App.css';
+import engagement from './images/engagement.jpg'
+import foliage from './images/foliage.png'
 
 const App = () => {
   return (
-    <div className="App">
+    <div className="app">
+      <div>
+        <img src={ foliage } alt="Foliage photo" className="banner-image"/>
+      </div>
+      <img src={ engagement } alt="Puppy engagement photo" className="main-image"/>
+      <div className="side-panel" id="left-sp"></div>
+      <div className="side-panel" id="right-sp"></div>
     </div>
   );
 }
