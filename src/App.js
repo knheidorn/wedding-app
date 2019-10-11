1942c13ee9be1e151ca12d78b50ea4d3bfb56a51
Kimberly Heidorn <heidorkn@gmail.com>
2019-10-11 14:39:28 -0700

    Building pages with styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React from 'react';
+import { Box } from '@material-ui/core'
+import { BrowserRouter as Switch, Router } from "react-router-dom";
+
+import Toolbar from './components/Toolbar'
 import './App.css';
 import engagement from './images/engagement.jpg'
 import foliage from './images/foliage.png'
 
+
 const App = () => {
-  return (
-    <div className="app">
-      <div>
-        <img src={ foliage } alt="Foliage photo" className="banner-image"/>
+  return(
+    <Router>
+      <div className="app">
+        <header className="header-style">
+          <Box>
+            <img src={ foliage } alt="Foliage" className="banner-image"/>
+          </Box>
+          <Toolbar />
+        </header>
+        <Box>
+          <img src={ engagement } alt="Puppy engagement photo" className="main-image"/>
+        </Box>
       </div>
-      <img src={ engagement } alt="Puppy engagement photo" className="main-image"/>
-      <div className="side-panel" id="left-sp"></div>
-      <div className="side-panel" id="right-sp"></div>
-    </div>
+    </Router>
   );
 }
 
---
+++ src/components/Toolbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Toolbar = () => {
+  return(
+    
+  )
+}
+
+export default Toolbar;
