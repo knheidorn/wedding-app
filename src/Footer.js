fcb75aeecabdb2c6de6d3438244f197921f02f56
Kimberly Heidorn <heidorkn@gmail.com>
2019-11-01 12:21:00 -0700

    Updated URL for backend Address form and start build out for Footer

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div id="footer">
+      Designed and built with love by the future Mrs. Weinheimer
+    <div>
+  )
+}
+export default Footer
--- src/components/Address.js
+++ src/components/Address.js
@@ -49,9 +49,8 @@ const Address = () => {
       body: JSON.stringify(body)
     }
 
-    // fetch("https://localhos", config)
-    //   .then(response => response.json())
-    //   .then(data => {console.log(data)})
+    fetch("https://guest-book-wein.herokuapp.com/guests", config)
+      .then(response => response.json())
   }
 
   const submitForm = () => {
